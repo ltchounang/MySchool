2a1f2646d4797bbaf880ab54839a82802b8ba68e
ltchounang <49473172+ltchounang@users.noreply.github.com>
2020-03-22 11:56:44 +0100

    version1 revu et corrigé
    il manque les répertoire jquerry, bootstrap-icons , bootstrap-css et bootstrap pour que tout le code fonctionne mais il est possible de remplacer les liens présent dans import par les liens en ligne pour que le code fonctionne.
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>

--- index.php
+++ index.php
@@ -1,64 +1,39 @@
 <?php
-/*
-    On active l'affichage des erreurs
-*/
-error_reporting(E_ALL);
-ini_set('display_errors', TRUE);
-ini_set('display_startup_errors', TRUE);
-
-// On définit une constante pour bien obliger les utilisateurs à passer par l'index
-if(!isset($_SESSION['login']) && !defined('CONSTANT')) {
+//sécurité : on definie une constante pour bien obliger les utilisateur à passer par l'index
+if(!isset($_SESSION['login']) && !defined('CONSTANT')){
     session_start();
-    define('CONSTANT', NULL);
-}
-require_once('vue_generique.php');   
-require_once('modules/mod_etudiant/mod_etudiant.php');  
-require_once('modules/mod_connexion/mod_connexion.php');
-
-
-ModeleConnexion::initConnexion(); // On initialise la connexion
-
-if (isset($_GET['module'])) {
-    $moduleChoisi = htmlspecialchars($_GET['module']);
-    $classe = 'mod' . $moduleChoisi;
-    $module = new $classe();
+    define('CONSTANT',NULL);
 }
 
-else{
-    $module = new ModEtudiant();
-}
+class formAjoutEtudException extends Exception {};
+class formModifEtudException extends Exception {};
+class formCoException extends Exception {};
+class formInsException extends Exception {};
 
-if (!isset($_SESSION['login'])) {
-    $lienConnect = '<li class="nav-item">
-    <a class="nav-link" href="index.php?module=connexion&action=form_connexion"><img class="logosNav" src="ressources/imgSite/logos/nav/logo_connexion.png" alt="logoConnexion"/>Connexion</a>
-    </li>';
+try {
+    if(isset($_SESSION['monid'])){
+        if(isset($_GET['module']) ){
+            if($_GET['module']=='etudiant'){
+                require_once('mod_etudiant/mod_etudiant.php');
+            }
+            elseif($_GET['module']=='connexion'){
+                require_once('mod_connexion/mod_connexion.php');
+            }
+            else{
+                echo 'module inconnu par où êtes vous passé ?';
+            }
+        }
+        else{
+            require_once('mod_connexion/mod_connexion.php');
+        }
     }
-    else {
-      $lienConnect = '<!-- Dropdown -->
-      
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
-          aria-haspopup="true" aria-expanded="false">
-          
-          ' . $_SESSION['login'] .'
-            <img src="https://mdbootstrap.com/img/Photos/Avatars/avatar-5.jpg" class="rounded-circle z-depth-0"
-            alt="avatar image" height="25"></a>
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-          <a class="dropdown-item" href="index.php?module=connexion&action=form_responsable"><img class="logosNav" src="ressources/imgSite/logos/nav/logo_ajouter_responsable.png" alt="logoConnexion"/>Ajouter un responsable </a>
-          <a class="dropdown-item" href="#"><img class="logosNav" src="ressources/imgSite/logos/nav/logo_espace_membre.png" alt="logoGestion"/>Espace membre</a>
-          
-          <div class="dropdown-divider"></div>
-    <a class="dropdown-item" href="index.php?module=connexion&action=seDeconnecter"><img class="logosNav" src="ressources/imgSite/logos/nav/logo_deconnexion.png" alt="logoConnexion"/> Déconnexion </a>
-  </div>
-        </div>
-      </li>';
-      
-      
-      
-    
+    else{
+        require_once('mod_connexion/mod_connexion.php');
+    }
 }
-
-            
-$content = VueGenerique::getAffichage();
-require('template.php');
-?>
+catch(Exception $e){
+    echo $e->getMessage();
+}
+finally {
+    require_once('mod_erreur/mod_erreur.php');
+}
