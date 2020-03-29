5e54d911904d4105012877be9fd3921f80dbd8b1
Yanniiss <49472846+Yanniiss@users.noreply.github.com>
2020-03-29 17:58:10 +0200

    Add files via upload

--- template.php
+++ template.php
@@ -22,10 +22,11 @@ if(!defined ('CONSTANT') )
 			<!-- menu des options à gauche -->
 			<aside class="col-xs-2 col-sm-2 col-md-2 col-lg-2" >
 				<h2 class="text-light"> menu option </h2>
-				<a class="btn optionMenu" href="index.php?module=etudiant&action=listeEtudiant" role="button" > liste etudiant  <img src="bootstrap-icons/icons/people-fill.svg" alt="..." width="32" height="32" title="People fill"> </a>
-				<a class="btn optionMenu" href="index.php?module=etudiant&action=formAjoutEtud" role="button" > ajouter etudiant <img src="bootstrap-icons/icons/plus.svg" alt="+" width="32" height="32" title="Plus"> </a>
+				<a class="btn optionMenu" href="index.php?module=etudiant&action=listeEtudiant" role="button" > liste etudiant  <img src="bootstrap-icons/icons/people-fill.svg" alt="..." width="32" height="32"> </a>
+				<a class="btn optionMenu" href="index.php?module=etudiant&action=formAjoutEtud" role="button" > ajouter etudiant <img src="bootstrap-icons/icons/plus.svg" alt="+" width="32" height="32"> </a>
 				
-				<a class="btn optionMenu"  href="index.php?module=etudiant&action=importer_fichier" role="button" > Importer fichier excel <img src="ressources/imgSite/logos/gestion/import_logo.png" alt="..." width="32" height="32" title="import"> </a>
+				<a class="btn optionMenu"  href="index.php?module=etudiant&action=importer_fichier" role="button" > Importer fichier excel <img src="ressources/imgSite/logos/gestion/import_logo.png" alt="..." width="32" height="32"> </a>
+
 			</aside>
 
 			<!-- contenu au centre -->
